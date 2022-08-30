6142e63efa6ce373a6624ea85dbea9a1c7249848
Henry Gillinger <henry.gillinger@ga.ntig.se>
2022-08-30 10:28:40 +0200

    initial commit

---
+++ Lucktext2/Lucktext2/Program.cs
@@ -0,0 +1,21 @@
+﻿string name; 
+Console.WriteLine("whats your name?");
+name = Console.ReadLine( ); 
+
+string leg; 
+Console.WriteLine("type a bodypart");
+leg=Console.ReadLine ( ); 
+
+string food; 
+Console.WriteLine("whats your favorite food?");
+food=Console.ReadLine( ); 
+
+
+Console.WriteLine($"hej {name}");
+// Console.WriteLine("name");
+Console.WriteLine($"if you have hurt your {leg} you should eat some {food} too feel better :=)");
+
+Console.ReadLine(); 
+
+
+
