81a570e8bbca522a909f7348613a7f2d622d1dbf
Henry Gillinger <henry.gillinger@ga.ntig.se>
2022-08-30 10:28:48 +0200

    liugyligyu

---
+++ interaktive/Program.cs
@@ -0,0 +1,3 @@
+string name; 
+Console.WriteLine("hello adventurer, the local baker has been kidnapped and you have to save im in order to give bread to all the people of the land. Some people think he was taken to the woods, and other people think he was taken across the bridge to the castle. Where would you like to go?");
+ReadLine ( );   
